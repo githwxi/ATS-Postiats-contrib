24447ce4f839effff10cd2817acc6b41fda1de01
Hongwei Xi <gmhwxi@gmail.com>
2014-09-03 13:58:18 -0400

    Updating: minorly fixing

--- projects/SMALL/GameOf24/PHP/mytest.php
+++ projects/SMALL/GameOf24/PHP/mytest.php
@@ -2,16 +2,16 @@
 //
 include './GameOf24_php_dats.php';
 //
-require './libatscc2php/prelude_basics_cats.php';
-require './libatscc2php/prelude_integer_cats.php';
+require './libatscc2php/CATS/basics_cats.php';
+require './libatscc2php/CATS/integer_cats.php';
 //
-require './libatscc2php/prelude_float_cats.php';
-require './libatscc2php/prelude_string_cats.php';
-require './libatscc2php/prelude_filebas_cats.php';
+require './libatscc2php/CATS/float_cats.php';
+require './libatscc2php/CATS/string_cats.php';
+require './libatscc2php/CATS/filebas_cats.php';
 //
-require './libatscc2php/prelude_PHParref_cats.php';
+require './libatscc2php/CATS/PHParref_cats.php';
 //
-require './libatscc2php/prelude_list_dats.php';
+require './libatscc2php/DATS/list_dats.php';
 //
 GameOf24_php_main0_php();
 //
