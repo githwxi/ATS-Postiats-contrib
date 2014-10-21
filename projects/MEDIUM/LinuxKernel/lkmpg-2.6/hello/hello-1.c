e9e533863400abce7fb61ea278a35c4c6bc4ddba
Hongwei Xi <gmhwxi@gmail.com>
2014-10-21 02:46:22 -0400

    Updating: working on lkmpg-2.6

---
+++ projects/MEDIUM/LinuxKernel/lkmpg-2.6/hello/hello-1.c
@@ -0,0 +1,26 @@
+/*  
+** hello-1.c
+** The simplest kernel module.
+*/
+/* Needed by all */
+#include <linux/module.h>
+/* Needed for KERN_INFO */
+#include <linux/kernel.h>
+
+int init_module(void)
+{
+  printk(KERN_INFO "Hello world 1.\n");
+
+/* 
+** A non-zero return means init_module failed;
+** as a consequence, the module cannot be loaded. 
+*/
+  return 0;
+}
+
+void cleanup_module(void)
+{
+  printk(KERN_INFO "Goodbye world 1.\n");
+}
+
+/* end of [hello-2.c] */
