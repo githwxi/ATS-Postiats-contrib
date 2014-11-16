16d9be5bd2f1f427e187d9058196da81c9e47cca
Hongwei Xi <gmhwxi@gmail.com>
2014-11-16 16:29:41 -0500

    Updating: improving libatscc2js

---
+++ contrib/libatscc/libatscc2pl/CATS/basics_cats.pl
@@ -0,0 +1,41 @@
+######
+#
+# HX-2014-11:
+# for Perl code translated from ATS
+#
+######
+
+######
+#beg of [basics_cats.pl]
+######
+
+############################################
+
+sub
+ATSCKiseqz { return ($_[0] == 0); }
+sub
+ATSCKisneqz { return ($_[0] != 0); }
+
+############################################
+
+sub
+ATSCKptrisnull { return ($_[0] == 0); }
+sub
+ATSCKptriscons { return ($_[0] != 0); }
+
+############################################
+
+sub
+ATSCKpat_int { return ($_[0] == $_[1]); }
+sub
+ATSCKpat_bool { return ($_[0] == $_[1]); }
+sub
+ATSCKpat_char { return ($_[0] == $_[1]); }
+sub
+ATSCKpat_float { return ($_[0] == $_[1]); }
+
+############################################
+
+######
+#end of [basics_cats.js]
+######
