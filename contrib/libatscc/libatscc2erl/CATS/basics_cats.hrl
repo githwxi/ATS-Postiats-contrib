33c8aa3478b55da47eab76f5f4551550f3b21533
Hongwei Xi <gmhwxi@gmail.com>
2015-07-03 13:10:42 -0400

    Updating: improving on libatscc

---
+++ contrib/libatscc/libatscc2erl/CATS/basics_cats.hrl
@@ -0,0 +1,26 @@
+%%
+%%%%%%
+%
+% HX-2015-07:
+% for Erlang code
+% translated from ATS
+%
+%%%%%%
+%%
+
+%%
+%%%%%%
+% beg of [basics_cats.hrl]
+%%%%%%
+%%
+
+%% ****** ****** %%
+
+%%fun%%
+ats2erl_ATSCKiseqz(X) -> (X =:= 0).
+%%fun%%
+ats2erl_ATSCKisneqz(X) -> (X =/= 0).
+
+%% ****** ****** %%
+
+%% end of [basics_cats.hrl] %%
