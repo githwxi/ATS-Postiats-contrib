8abaf70610a006621812b01cc169d2274f8fa27e
Hongwei Xi <gmhwxi@gmail.com>
2015-09-05 01:41:40 -0400

    Updating: adding libatscc2erl/Erlang

---
+++ contrib/libatscc/libatscc2erl/CATS/Erlang/file_cats.hrl
@@ -0,0 +1,19 @@
+%%
+%%%%%%
+%
+% HX-2015-09:
+% for Erlang code
+% translated from ATS
+%
+%%%%%%
+%%
+
+%%
+%%%%%%
+% beg of [file_cats.hrl]
+%%%%%%
+%%
+
+%% ****** ****** %%
+
+%% end of [file_cats.hrl] %%
