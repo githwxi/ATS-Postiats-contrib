8b6ca45ee719b69c1f91446d35226145c0211eb7
Hongwei Xi <gmhwxi@gmail.com>
2016-05-30 17:07:53 -0400

    Updating: very minorly improving

--- contrib/libatscc/libatscc2scm/CATS/basics_cats.scm
+++ contrib/libatscc/libatscc2scm/CATS/basics_cats.scm
@@ -7,7 +7,11 @@
 ;
 ;;;;;;
 ;;
-
+;; For some implementations of scheme,
+;; we may have to map [define-macro] to [defmacro]
+;;
+;; (defmacro (define-macro x y) `(defmacro ,x ,y))
+;;
 ;;;;;;
 
 ;;;;;; end of [basics_cats.scm] ;;;;;;
