9e4c216685e472422e2b28980ac318068ecdb2ea
Hongwei Xi <gmhwxi@gmail.com>
2016-06-01 14:31:40 -0400

    Updating: very minorly

---
+++ contrib/libatscc/libatscc2scm/CATS/print_cats.scm
@@ -0,0 +1,37 @@
+;;
+;;;;;;
+;
+; HX-2016-06:
+; for Scheme code
+; translated from ATS
+;
+;;;;;;
+;;
+
+;;
+;;;;;;
+; beg of [print_cats.scm]
+;;;;;;
+;;
+
+;; ****** ****** ;;
+;;
+(define-macro
+ ats2scmpre_print_int(x) `(display ,x))
+;;
+(define-macro
+ ats2scmpre_print_char(x) `(display ,x))
+(define-macro
+ ats2scmpre_print_bool(x) `(display (if ,x "true" "false"))
+;;
+(define-macro
+ ats2scmpre_print_double(x) `(display ,x))
+(define-macro
+ ats2scmpre_print_string(x) `(display ,x))
+;;
+(define-macro
+ (ats2scmpre_print_SCMval x) `(display ,x))
+;;
+;; ****** ****** ;;
+
+;; end of [print_cats.scm] ;;
