5e38c32b31f376732743c6be9b5bd865e3a7ffa1
Hongwei Xi <gmhwxi@gmail.com>
2016-07-15 23:24:57 -0400

    Updating: very very minorly

---
+++ contrib/libatscc/libatscc2clj/CATS/bool_cats.clj
@@ -0,0 +1,19 @@
+;;
+;;;;;;
+;
+; HX-2016-07:
+; for Clojure code
+; translated from ATS
+;
+;;;;;;
+;;
+
+;;
+;;;;;;
+; beg of [bool_cats.clj]
+;;;;;;
+;;
+
+;; ****** ****** ;;
+
+;; end of [bool_cats.clj] ;;
