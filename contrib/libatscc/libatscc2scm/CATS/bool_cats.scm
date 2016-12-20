cc391bf381db640fce32711f14a2e8b8bb16c453
Hongwei Xi <gmhwxi@gmail.com>
2016-12-19 23:22:10 -0500

    Updating: very very minorly

--- contrib/libatscc/libatscc2scm/CATS/bool_cats.scm
+++ contrib/libatscc/libatscc2scm/CATS/bool_cats.scm
@@ -16,4 +16,13 @@
 
 ;; ****** ****** ;;
 
+(define-macro
+ (ats2scmpre_neg_bool0 x) `(not ,x)
+)
+(define-macro
+ (ats2scmpre_neg_bool1 x) `(not ,x)
+)
+
+;; ****** ****** ;;
+
 ;; end of [bool_cats.scm] ;;
